150437a4166e8dcfffc0382196d59eb5c17a08ed
ozzaynetworks <ozzaynetworks@gmail.com>
2020-09-21 06:52:47 +0300

    about us

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,161 +1,215 @@
-//Bootstrap Select
 
-jQuery(document).ready(function( $ ) {
-$('.selectpicker').selectpicker({
-  style: 'btn-select',
-  size: 4
-});
-});
+/*
+Theme Name:       Classix - Classified Ads and Listing Template
+Author:           UIdeck
+Author URI:       http://uideck.com
+Text Domain:      UIdeck
+Domain Path:      /languages/
 
+JS INDEX
+================================================
+1. preloader js
+2. scroll to top js
+3. slick menu js
+4. sticky menu js
+6. counter js
+9. wow js
+7. Testimonial owl carousel
+8. New Products owl carouse
+9. Categories Iocn owl Carousel
+================================================*/
 
-//CounterUp
-jQuery(document).ready(function( $ ) {
-  $('.counter').counterUp({
-    delay: 1,
-    time: 800
-  });
-});
+(function($) {
 
-//WOW Scroll Spy
-var wow = new WOW({
-    //disabled for mobile
-    mobile: false
-});
-wow.init();
+  var $main_window = $(window);
 
-// Page Loader
-$(window).load(function() {
-  "use strict";
-  $('#loader').fadeOut();
-});
-
-
-/* ==========================================================================
-   New Products Owl Carousel
-   ========================================================================== */
-  $("#new-products").owlCarousel({
-      navigation: true,
-      pagination: true,
-      slideSpeed: 1000,
-      stopOnHover: true,
-      autoPlay: true,
-      items: 5,
-      itemsDesktopSmall: [1024, 3],
-      itemsTablet: [600, 1],
-      itemsMobile: [479, 1]
+  /*====================================
+  preloader js
+  ======================================*/
+  $main_window.on("load", function() {
+      $("#preloader").fadeOut("slow");
   });
 
-/* ==========================================================================
-   Touch Owl Carousel
-   ========================================================================== */
-$(".touch-slider").owlCarousel({
-    navigation: false,
-    pagination: true,
-    slideSpeed: 1000,
-    stopOnHover: true,
-    autoPlay: true,
-    items: 1,
-    itemsDesktopSmall: [1024, 1],
-    itemsTablet: [600, 1],
-    itemsMobile: [479, 1]
-});
+  /*====================================
+  scroll to top js
+  ======================================*/
+  $main_window.on("scroll", function() {
+    if ($(this).scrollTop() > 250) {
+      $(".back-to-top").fadeIn(200);
+    } else {
+      $(".back-to-top").fadeOut(200);
+    }
+  });
+  $(".back-to-top").on("click", function() {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      "slow"
+    );
+    return false;
+  });
+    
+  /*====================================
+  slick menu js
+  ======================================*/
+  var logo_path=$('.mobile-menu').data('logo');
+  $('#main-navbar').slicknav({
+      appendTo:'.mobile-menu',
+      removeClasses:false,
+      label:'',
+      closedSymbol:'<i class="lni-chevron-right"><i/>',
+      openedSymbol:'<i class="lni-chevron-right"><i/>',
+      brand:'<a href="index.html"><img src="'+logo_path+'" class="img-responsive" alt="logo"></a>'
+  });
 
-$('.touch-slider').find('.owl-prev').html('<i class="fa fa-angle-left"></i>');
-$('.touch-slider').find('.owl-next').html('<i class="fa fa-angle-right"></i>');
+  /*====================================
+  sticky menu js
+  ======================================*/
+  $main_window.on('scroll', function () {  
+    var scroll = $(window).scrollTop();
+    if (scroll >= 10) {
+        $(".scrolling-navbar").addClass("top-nav-collapse");
+    } else {
+        $(".scrolling-navbar").removeClass("top-nav-collapse");
+    }
+  });
 
-$('#new-products').find('.owl-prev').html('<i class="fa fa-angle-left"></i>');
-$('#new-products').find('.owl-next').html('<i class="fa fa-angle-right"></i>');
+  /*=======================================
+  counter
+  ======================================= */
+  if ($(".counter").length > 0) {
+    $(".counterUp").counterUp({
+      delay: 10,
+      time: 2000
+    });
+  }
 
-var owl;
+  /*====================================
+  wow js
+  ======================================*/
+  var wow = new WOW({
+      //disabled for mobile
+      mobile: false
+  });
+  wow.init();
 
-$(document).ready(function () {
+  /*====================================
+  Tooltip Toggle
+  ======================================*/
+  $('[data-toggle="tooltip"]').tooltip()
 
-    owl = $("#owl-demo");
+  /*====================================
+  Testimonials Carousel 
+  ======================================*/
+  var testiOwl = $("#testimonials");
+  testiOwl.owlCarousel({
+      autoplay:true,
+      margin:30,
+      dots:false,
+      autoplayHoverPause:true,
+      nav:false,
+      loop:true,
+      responsiveClass:true,
+      responsive:{
+          0: {
+              items:1,
+          },
+          991:{
+              items:2
+        }
+      }
+  });
 
-    owl.owlCarousel({
-
-        navigation: false, // Show next and prev buttons
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        singleItem: true,
-        afterInit: afterOWLinit, // do some work after OWL init
-        afterUpdate : afterOWLinit
+  /*====================================
+  New Products Owl Carousel
+  ======================================*/
+  var newproducts = $("#new-products");
+    newproducts.owlCarousel({
+      autoplay: true,
+      nav: true,
+      autoplayHoverPause:true,
+      smartSpeed: 350,
+      dots: false,
+      margin:30,
+      loop: true,
+      navText: [
+        '<i class="lni-chevron-left"></i>',
+        '<i class="lni-chevron-right"></i>'
+      ],
+      responsiveClass: true,
+      responsive: {
+          0: {
+              items: 1,
+          },
+          575: {
+              items: 2,
+          },
+          991: {
+              items: 3,
+          }
+        }
     });
 
-    function afterOWLinit() {
-        // adding A to div.owl-page
-        $('.owl-controls .owl-page').append('<a class="item-link" />');
-        var pafinatorsLink = $('.owl-controls .item-link');
-        /**
-         * this.owl.userItems - it's your HTML <div class="item"><img src="http://www.ow...t of us"></div>
-         */
-        $.each(this.owl.userItems, function (i) {
-
-            $(pafinatorsLink[i])
-                // i - counter
-                // Give some styles and set background image for pagination item
-                .css({
-                    'background': 'url(' + $(this).find('img').attr('src') + ') center center no-repeat',
-                    '-webkit-background-size': 'cover',
-                    '-moz-background-size': 'cover',
-                    '-o-background-size': 'cover',
-                    'background-size': 'cover'
-                })
-                // set Custom Event for pagination item
-                .click(function () {
-                    owl.trigger('owl.goTo', i);
-                });
-
-        });
-         // add Custom PREV NEXT controls
-        $('.owl-pagination').prepend('<a href="#prev" class="prev-owl"/>');
-        $('.owl-pagination').append('<a href="#next" class="next-owl"/>');
-        // set Custom event for NEXT custom control
-        $(".next-owl").click(function () {
-            owl.trigger('owl.next');
-        });
-        // set Custom event for PREV custom control
-        $(".prev-owl").click(function () {
-            owl.trigger('owl.prev');
-        });
-    }
-});
-
- // Back Top Link
-  var offset = 200;
-  var duration = 500;
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > offset) {
-      $('.back-to-top').fadeIn(400);
-    } else {
-      $('.back-to-top').fadeOut(400);
-    }
+  /*====================================
+  Categories Iocn Owl Carousel
+  ======================================*/
+  var categoriesslider = $("#categories-icon-slider");
+  categoriesslider.owlCarousel({
+    autoplay: true,
+    nav: false,
+    autoplayHoverPause:true,
+    smartSpeed: 350,
+    dots: true,
+    margin:30,
+    loop: true,
+    navText: [
+      '<i class="lni-chevron-left"></i>',
+      '<i class="lni-chevron-right"></i>'
+    ],
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        575: {
+            items: 2,
+        },
+        991: {
+            items: 5,
+        }
+      }
   });
-  $('.back-to-top').click(function(event) {
-    event.preventDefault();
-    $('html, body').animate({
-      scrollTop: 0
-    }, 600);
-    return false;
-  })
 
- $('.list,switchToGrid').click(function(e) {
-    e.preventDefault();
-    $('.grid').removeClass("active");
-    $('.list').addClass("active");
-    $('.item-list').addClass("make-list");
-    $('.item-list').removeClass("make-grid");
-    $('.item-list').removeClass("make-compact");
-    $('.item-list .add-desc-box').removeClass("col-sm-9");
-    $('.item-list .add-desc-box').addClass("col-sm-7");
+  /*====================================
+  Details  Owl Carousel
+  ======================================*/
+  var detailsslider = $("#owl-demo");
+  detailsslider.owlCarousel({
+    autoplay: true,
+    nav: false,
+    autoplayHoverPause:true,
+    smartSpeed: 350,
+    dots: true,
+    margin:30,
+    loop: true,
+    navText: [
+      '<i class="lni-chevron-left"></i>',
+      '<i class="lni-chevron-right"></i>'
+    ],
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        575: {
+            items: 1,
+        },
+        991: {
+            items: 1,
+        }
+      }
   });
-  $('.grid').click(function(e) {
-    e.preventDefault();
-    $('.list').removeClass("active");
-    $(this).addClass("active");
-    $('.item-list').addClass("make-grid");
-    $('.item-list').removeClass("make-list");
-    $('.item-list').removeClass("make-compact");
-    $('.item-list .add-desc-box').removeClass("col-sm-9");
-    $('.item-list .add-desc-box').addClass("col-sm-7");
-  });
+
+    
+})(jQuery);
